f9493cdeaeea723831f8e6b30832b1269d3b36e4
Benj Prosnitz <bprosnitz@bprosnitz-goo.sin.corp.google.com>
2016-07-27 17:58:53 +0800

    added string items

--- go/src/bprosnitz/dexterity/dexterity.go
+++ go/src/bprosnitz/dexterity/dexterity.go
@@ -22,102 +22,114 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
-  if err := processDex(f); err != nil {
+  dex := &Dex{}
+  if err := readDex(f, dex); err != nil {
     log.Fatal(err)
   }
+  fmt.Printf("%#v\n", dex)
   f.Close()
 }
 
-func processDex(r io.Reader) error {
-  header, err := readDexHeader(r)
+func readDex(r io.Reader, dex *Dex) error {
+  err := readDexHeader(r, &dex.Header)
   if err != nil {
     return err
   }
-  fmt.Printf("header: %#v\n", header)
+
+  dex.StringIds = make([]DexStringIdItem, dex.Header.StringIdsSize)
+  for i := 0; i < int(dex.Header.StringIdsSize); i++ {
+    if err := read(r, &dex.StringIds[i]); err != nil {
+      return err
+    }
+  }
 
   return nil
 }
 
-func readDexHeader(r io.Reader) (*DexHeader, error) {
+func readDexHeader(r io.Reader, header *DexHeader) error {
   readMagic := make([]byte, 8)
   if _, err := r.Read(readMagic); err != nil {
-    return nil, err
+    return err
   }
   if string(readMagic) != dexMagic {
-    return nil, fmt.Errorf("invalid magic string %q", string(readMagic))
+    return fmt.Errorf("invalid magic string %q", string(readMagic))
   }
 
-  header := &DexHeader{}
   if err := read(r, &header.Checksum); err != nil {
-    return nil, err
+    return err
   }
   if n, err := r.Read(header.Signature[:]); err != nil {
-    return nil, err
+    return err
   } else if n != 20 {
-    return nil, fmt.Errorf("partial signature bytes read")
+    return fmt.Errorf("partial signature bytes read")
   }
   if err := read(r, &header.FileSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.HeaderSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.EndianTag); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.LinkSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.LinkOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.MapOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.StringIdsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.StringIdsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.TypeIdsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.TypeIdsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.ProtoIdsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.ProtoIdsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.FieldIdsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.FieldIdsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.MethodIdsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.MethodIdsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.ClassDefsSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.ClassDefsOff); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.DataSize); err != nil {
-    return nil, err
+    return err
   }
   if err := read(r, &header.DataOff); err != nil {
-    return nil, err
+    return err
   }
 
-  return header, nil
+  return nil
+}
+
+type Dex struct {
+  Header DexHeader
+  StringIds []DexStringIdItem
 }
 
 type DexHeader struct {
@@ -143,6 +155,10 @@ type DexHeader struct {
   ClassDefsOff uint32
   DataSize uint32
   DataOff uint32
+}
+
+type DexStringIdItem struct {
+  StringDataOff uint32
 }
 
 
